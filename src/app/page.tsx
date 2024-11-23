260ed2533d8affe040e82e0cf5d61973a0b74f19
Oluyemi29 <adedokunoluyemi29@gmail.com>
2024-11-23 18:52:39 +0100

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,44 @@
+"use client";
 import { CardDemo } from "@/components/Card";
+import { CardRight } from "@/components/CardRight";
 import Chart from "@/components/Chart";
 import Notify from "@/components/Notify";
 import { SkeletonCard } from "@/components/Skeleton";
 import { SliderDemo } from "@/components/Slider";
+import Image from "next/image";
+import Picture from "@/image/bus.jpg";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello</h1>
+      <div className="flex my-4 justify-between md:flex-row flex-col">
+        <motion.div
+          className="w-full"
+          initial={{ y: -400, opacity: 0 }}
+          animate={{
+            y: 0,
+            transition: {
+              duration: 2,
+              delay: 1,
+              type: "spring",
+              stiffness: 500,
+            },
+            opacity: 1,
+          }}
+        >
+          <Image
+            src={Picture}
+            alt="bus"
+            width={100}
+            height={100}
+            className="w-full md:w-[50%] m-auto"
+          />
+        </motion.div>
+        <div className="w-full">
+          <CardRight />
+        </div>
+      </div>
       <div className="grid md:grid-cols-3 grid-cols-1 gap-7">
         <CardDemo />
         <CardDemo />
--- src/components/Notify.tsx
+++ src/components/Notify.tsx
@@ -24,9 +24,9 @@ const Notify = () => {
                 className="h-6 w-6"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 />
               </svg>
             </div>
@@ -69,9 +69,9 @@ const Notify = () => {
                 className="h-6 w-6"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 />
               </svg>
             </div>
@@ -112,9 +112,9 @@ const Notify = () => {
                 className="h-6 w-6"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 />
               </svg>
             </div>
